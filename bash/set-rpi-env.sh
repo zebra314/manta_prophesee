b6ee77bc977419d87a747c6020e036a6a7fd80be
zebra314 <kidalfonso@gmail.com>
2025-11-16 07:21:02 +0800

    Add set-rpi-env.sh script for environment setup

--- bash/set-rpi-env.sh
+++ bash/set-rpi-env.sh
@@ -1,3 +1,5 @@
+# !/bin/bash
+# Execute this script using: source set-rpi-env.sh
 source ~/openeb/build/utils/scripts/setup_env.sh
 source ~/prophesee_venv/bin/activate
 export LIBGL_ALWAYS_SOFTWARE=1
